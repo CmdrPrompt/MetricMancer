04186dd8e6e130baccf5ed6107f701de5956fd88
Thomas Lindqvist <lindqvist.t@gmail.com>
2025-10-09 11:03:04 +0000

    PEP8 fixes

--- src/kpis/complexity/kpi.py
+++ src/kpis/complexity/kpi.py
@@ -21,5 +21,5 @@ class ComplexityKPI(BaseKPI):
         """
         self.value = complexity
         self.calculation_values = {"function_count": function_count}
-        return self
-        
+    return self
+
