7c9f3ddba8027aee39f5ebf8ed868c7288e386b3
Commander Prompt <lindqvist.t@gmail.com>
2026-03-05 11:26:53 +0000

    refactor(coupling): export new KPI classes from coupling module

--- src/kpis/coupling/__init__.py
+++ src/kpis/coupling/__init__.py
@@ -7,5 +7,12 @@ based on commit history, following Adam Tornhill's methodology from
 """
 
 from .coupling_analyzer import CouplingAnalyzer, CouplingData
+from .logical_coupling_kpi import LogicalCouplingKPI
+from .change_coupled_hotspot_kpi import ChangeCoupledHotspotKPI
 
-__all__ = ['CouplingAnalyzer', 'CouplingData']
+__all__ = [
+    'CouplingAnalyzer',
+    'CouplingData',
+    'LogicalCouplingKPI',
+    'ChangeCoupledHotspotKPI'
+]
